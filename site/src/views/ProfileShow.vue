e1cd1f3f8be28a4cafffb7bebbe42b0294e345eb
Unaxe <74629449+Unaxe@users.noreply.github.com>
2022-01-07 09:26:07 +0100

    Profile Show modification

--- site/src/views/ProfileShow.vue
+++ site/src/views/ProfileShow.vue
@@ -1,47 +1,51 @@
 <template>
-    <div class="mx-auto pb-6 text-1 md:flex md:space-x-4s">
-        <div class=" md:w-1/2 lg:w-2/3 m-4 space-y-4">
-            <div class="card p-0 pb-6">
-                <div class="relative h-48 w-full">
-                    <div class="banner w-full h-full rounded-t-md" />
-                    <!-- <div class="flex absolute top-0 bg-gray-500 opacity-50 rounded items-center justify-center w-full h-full">
+    <div class="mx-auto pb-6 text-1">
+        <div class="card rounded-b-none p-0 pb-6">
+            <div class="relative h-48 w-full">
+                <div class="banner w-full h-full rounded-t-md" />
+                <!-- <div class="flex absolute top-0 bg-gray-500 opacity-50 rounded items-center justify-center w-full h-full">
                 Changer votre banniere
               </div> -->
-                    <img
-                        :src="user.avatar"
-                        alt=""
-                        class="absolute right-10 -bottom-1/4 h-32 w-32 rounded-full"
-                    >
-                </div>
-                <div class="mt-20 px-4 w-full">
-                    <div class="flex flex-col pr-8 space-y-4 mb-4 ">
+                <img
+                    :src="user.avatar"
+                    alt=""
+                    class="absolute left-10 -bottom-1/4 h-32 w-32 rounded-full"
+                >
+            </div>
+            <div class="mt-20 px-4 w-full">
+                <div class="flex flex-col pr-8 space-y-4 mb-4 ">
+                    <div>
                         <div>
-                            <div>
-                                <div class="inline-block text-2xl">
-                                    {{ user.username }}
-                                </div>
-                                <div class="text-gray-500 inline-block ml-2 mb-4">
-                                    {{ 'M2-F' }}
-                                </div>
+                            <div class="inline-block text-2xl">
+                                {{ user.username }}
                             </div>
-                            <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam, impedit deserunt? Magni est atque sapiente temporibus quia, quaerat, voluptates qui fuga sequi deserunt, perferendis dolorem repellat. Rem quam molestias optio nesciunt similique illum repudiandae iure libero, ullam doloremque eius cupiditate voluptate tempora, maxime tenetur ea, facere impedit. Aliquid numquam consequuntur consequatur reiciendis aut sequi, neque, doloribus, quasi incidunt eum accusamus!</div>
+                            <div class="text-gray-500 inline-block ml-2 mb-4">
+                                {{ 'M2-F' }}
+                            </div>
                         </div>
+                        <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam, impedit deserunt? Magni est atque sapiente temporibus quia, quaerat, voluptates qui fuga sequi deserunt, perferendis dolorem repellat. Rem quam molestias optio nesciunt similique illum repudiandae iure libero, ullam doloremque eius cupiditate voluptate tempora, maxime tenetur ea, facere impedit. Aliquid numquam consequuntur consequatur reiciendis aut sequi, neque, doloribus, quasi incidunt eum accusamus!</div>
+                    </div>
 
-                        <div v-if="assos.length != 0">
-                            <div class="text-lg">
-                                Associations
-                            </div>
-                            <div
-                                v-for="asso in assos"
-                                :key="asso"
-                                class="flex mt-2"
-                            />
+                    <div v-if="assos.length != 0">
+                        <div class="text-lg">
+                            Associations
                         </div>
+                        <div
+                            v-for="asso in assos"
+                            :key="asso"
+                            class="flex mt-2"
+                        />
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
+    <div class="md:flex">
+        <div class=" md:w-1/2 lg:w-2/3 mt-0 m-4 space-y-4">
             <div class="flex flex-col card flex-grow space-y-4">
-                <div>Activité</div>
+                <div class="text-xl">
+                    Activité
+                </div>
                 <post-card-vue
                     v-for="activitie in activities"
                     :key="activitie.index"
@@ -99,7 +103,7 @@ export default {
 
 <style lang="scss">
 .banner {
-  background-color: #386376;
+  background-color: #771250;
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='60' height='96' viewBox='0 0 60 96'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%2335313c' fill-opacity='0.44'%3E%3Cpath d='M36 10a6 6 0 0 1 12 0v12a6 6 0 0 1-6 6 6 6 0 0 0-6 6 6 6 0 0 1-12 0 6 6 0 0 0-6-6 6 6 0 0 1-6-6V10a6 6 0 1 1 12 0 6 6 0 0 0 12 0zm24 78a6 6 0 0 1-6-6 6 6 0 0 0-6-6 6 6 0 0 1-6-6V58a6 6 0 1 1 12 0 6 6 0 0 0 6 6v24zM0 88V64a6 6 0 0 0 6-6 6 6 0 0 1 12 0v12a6 6 0 0 1-6 6 6 6 0 0 0-6 6 6 6 0 0 1-6 6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
 }
 </style>
